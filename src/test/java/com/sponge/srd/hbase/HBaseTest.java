db5a9f0e25d5a8636de8beca8ada362b43adbb3e
刘红波 <super_sponge@163.com>
2015-03-02 17:50:52 +0800

    add HBase query

--- src/test/java/com/sponge/srd/hbase/HBaseTest.java
+++ src/test/java/com/sponge/srd/hbase/HBaseTest.java
@@ -4,6 +4,11 @@ import com.sponge.srd.utils.HBaseUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.FilterList;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -57,8 +62,20 @@ public class HBaseTest {
             put.add("column1".getBytes(), null, "aaa".getBytes());// 本行数据的第一列
             put.add("column2".getBytes(), null, "bbb".getBytes());// 本行数据的第三列
             put.add("column3".getBytes(), null, "ccc".getBytes());// 本行数据的第三列
+            put.add(Bytes.toBytes("column1"), Bytes.toBytes("c1"),Bytes.toBytes("c1:aaa"));
 
-            table.put(put);
+
+            List lst =new  ArrayList();
+            lst.add(put);
+
+            put = new Put(Bytes.toBytes("rowkey2"));
+            put.add(Bytes.toBytes("column1"), Bytes.toBytes("c1"),Bytes.toBytes("c1:aaa"));
+            put.add("column1".getBytes(), null, "aaa".getBytes());// 本行数据的第一列
+            put.add("column2".getBytes(), null, "aaa".getBytes());// 本行数据的第一列
+
+            lst.add(put);
+
+            table.put(lst);
             table.close();
 
         } catch (IOException e) {
@@ -73,6 +90,7 @@ public class HBaseTest {
             HTable hTable = new HTable(HBaseUtils.getConfiguration(), tableName);
             List list = new ArrayList();
             Delete d1 = new Delete(rowkey.getBytes());
+            list.add(d1);
             hTable.delete(list);
             System.out.println("Delete succeed!");
 
@@ -84,6 +102,111 @@ public class HBaseTest {
 
     }
 
+    public static void queryAllData(String tableName){
+        try {
+            System.out.println("query all data ...");
+            HTable htable = new HTable(HBaseUtils.getConfiguration(), tableName);
+            ResultScanner rs  = htable.getScanner(new Scan());
+            for (Result r : rs) {
+                System.out.println("get rowkey:" + new String(r.getRow()));
+                dispCells(r.rawCells());
+            }
+
+            htable.close();
+        } catch (IOException e ){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void queryByKey(String tableName, String key){
+        try {
+            System.out.println("query " + tableName + " by row " + key);
+            HTable htable = new HTable(HBaseUtils.getConfiguration(), tableName);
+            Get get = new Get(Bytes.toBytes(key));
+            Result r = htable.get(get);
+            System.out.println("Result size is " + r.size());
+            dispCells(r.rawCells());
+            htable.close();
+
+        } catch (IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    the condition between filter is or . if the column is not exists, filter is true
+     */
+
+    public static void queryByConditionColumn(String tableName, String column, String value){
+        try {
+            System.out.println("query " + tableName + " by column " + column + " value " + value);
+            HTable htable = new HTable(HBaseUtils.getConfiguration(), tableName);
+            Filter filter = new SingleColumnValueFilter(Bytes.toBytes(column),
+                    null,
+                    CompareFilter.CompareOp.EQUAL, Bytes.toBytes(value));
+            Scan s = new Scan();
+            s.setFilter(filter);
+
+            ResultScanner rs = htable.getScanner(s);
+            for (Result r : rs){
+                System.out.println("get rowkey:" + new String(r.getRow()));
+                dispCells(r.rawCells());
+            }
+
+            htable.close();
+
+        }catch (IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void queryByConditionColumnCon(){
+        try {
+            System.out.println("query " + table);
+            HTable htable = new HTable(HBaseUtils.getConfiguration(), table);
+            List<Filter> filters = new ArrayList<Filter>();
+
+            Filter filter1 = new SingleColumnValueFilter(Bytes
+                    .toBytes("column1"), null, CompareFilter.CompareOp.EQUAL, Bytes
+                    .toBytes("aaa"));
+            filters.add(filter1);
+
+            Filter filter2 = new SingleColumnValueFilter(Bytes
+                    .toBytes("column2"), null, CompareFilter.CompareOp.EQUAL, Bytes
+                    .toBytes("bbb"));
+            filters.add(filter2);
+
+            Filter filter3 = new SingleColumnValueFilter(Bytes
+                    .toBytes("column3"), null, CompareFilter.CompareOp.EQUAL, Bytes
+                    .toBytes("ccc"));
+            filters.add(filter3);
+
+            FilterList filterList1 = new FilterList(filters);
+
+            Scan scan = new Scan();
+            scan.setFilter(filterList1);
+            ResultScanner rs = htable.getScanner(scan);
+            for (Result r : rs){
+                System.out.println("get rowkey:" + new String(r.getRow()));
+                dispCells(r.rawCells());
+            }
+
+            htable.close();
+
+        }catch (IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void dispCells(Cell[] cells){
+        for(Cell cell : cells) {
+            System.out.println("column： " + new String(CellUtil.cloneFamily(cell))
+                    + "    value: " + new String(CellUtil.cloneValue(cell)));
+        }
+    }
+
     @Before
     public void testcreateTable() {
         createTable(table);
@@ -92,6 +215,13 @@ public class HBaseTest {
     @Test
     public void testHBase() {
         insertData(table);
-        deleteData(table, rowkey);
+        queryAllData(table);
+        queryByKey(table, "rowkey");
+        queryByKey(table,"rowkey2");
+        queryByKey(table,"rowkey3");
+        queryByConditionColumn(table, "column1", "aaa");
+        queryByConditionColumn(table,"column3", "ccc");
+        queryByConditionColumnCon();
+        //deleteData(table, rowkey);
     }
 }
